21c8e6ebad7615053f48eae3f643f8e69e3c67e2
ThinkTec <ThinkTec@DESKTOP-95CAT2S>
2017-04-26 15:57:02 +0800

    ignore

---
+++ LogDataExtractTool/LogDataExtract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDataExtractTool
+{
+    class LogDataExtract
+    {
+        public string ssss { get; set; }
+        public static void ExtractData(string filePathName, string selectString)
+        {
+            if (!File.Exists(filePathName))
+            {
+                Console.WriteLine("{0} does not exist.", filePathName);
+                return;
+            }
+
+            StreamReader sr = File.OpenText(filePathName);
+            StreamWriter sw = File.CreateText(filePathName + "ex");
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if(line.Contains(selectString))
+                    sw.WriteLine(line);
+            }
+            Console.WriteLine("The end of the stream has been reached.");
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- LogDataExtractTool/Program.cs
+++ LogDataExtractTool/Program.cs
@@ -10,6 +10,29 @@ namespace LogDataExtractTool
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var filePath = "";
+                var selectString = "";
+                if (args.Length > 1)
+                {
+                    filePath = args[0];
+                    selectString = args[1];
+                }
+                else
+                {
+                    Console.WriteLine("Input File path and name.");
+                    filePath = Console.ReadLine();
+                    Console.WriteLine("Input string what you want to select.");
+                    selectString = Console.ReadLine();
+                }
+
+                filePath = filePath.Replace("\"",""); 
+
+                Console.WriteLine("Input File Path + Name : " + filePath);
+
+                LogDataExtract.ExtractData(filePath, selectString);
+             }
         }
     }
 }
